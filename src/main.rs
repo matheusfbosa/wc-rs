067218c24ba172d6ee27716fcddad4b12969f56e
Matheus Bosa <matheusfbosa@gmail.com>
2023-11-19 19:34:30 -0300

    feat: -m outputs the number of characters in a file

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,14 @@ use clap::Parser;
 #[derive(Parser)]
 #[command(author, version, about)]
 struct Cli {
-    #[arg(short('c'), long)]
+    #[arg(short = 'c', long)]
     bytes: bool,
     #[arg(short, long)]
     lines: bool,
     #[arg(short, long)]
     words: bool,
+    #[arg(short = 'm', long)]
+    chars: bool,
     file_path: PathBuf,
 }
 
@@ -26,22 +28,18 @@ fn main() {
         })
         .unwrap();
 
-    match (cli.bytes, cli.lines, cli.words) {
-        (true, _, _) => {
-            let bytes_count = content.len();
-            println!("{} {}", bytes_count, file_path.display());
-        }
-        (_, true, _) => {
-            let lines_count = content.lines().count();
-            println!("{} {}", lines_count, file_path.display());
-        }
-        (_, _, true) => {
-            let words_count = content.split_whitespace().count();
-            println!("{} {}", words_count, file_path.display());
-        }
+    match (cli.bytes, cli.lines, cli.words, cli.chars) {
+        (true, _, _, _) => print_count(content.len(), file_path),
+        (_, true, _, _) => print_count(content.lines().count(), file_path),
+        (_, _, true, _) => print_count(content.split_whitespace().count(), file_path),
+        (_, _, _, true) => print_count(content.chars().count(), file_path),
         _ => {
             eprintln!("error: please specify an argument");
             std::process::exit(1);
         }
     }
+}
+
+fn print_count(count: usize, file_path: PathBuf) {
+    println!("{} {}", count, file_path.display());
 }
