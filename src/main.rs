65c1aa3e94fbc640878a8723d1c2ea50bfbb723c
Matheus Bosa <matheusfbosa@gmail.com>
2023-11-19 18:55:31 -0300

    feat: -c outputs the number of bytes in a file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::fs;
+
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[arg(short, long)]
+    count_bytes: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    let file_path: String = cli.count_bytes;
+
+    let content: String = fs::read_to_string(file_path.clone()).unwrap_or_else(|err| {
+        eprintln!("error reading file '{}': {}", file_path, err);
+        std::process::exit(1);
+    });
+    let bytes_count = content.len();
+
+    println!("{} {}", bytes_count, file_path);
 }
